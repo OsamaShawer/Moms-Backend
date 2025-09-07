4d7e04cd28e4c6a9e258d384dfde9635f3ffe967
OsamaShawer <osamashawar7@gmail.com>
2025-09-07 11:41:14 +0300

    Applying cors to work with the backend

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const port = 3000;
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["https://mathtest-xi.vercel.app", "http://localhost:5173"],
   })
 );
 
